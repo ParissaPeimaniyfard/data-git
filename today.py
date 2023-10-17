27e18687a89ac08df3b228931dbf8f9056361a0d
Parissa Peimaniyfard <zahra.pmf@gmail.com>
2023-10-17 21:10:19 +0200

    Fix style issues, should get a 'Good Style' now 🙏

--- today.py
+++ today.py
@@ -1,11 +1,12 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
+
 def my_name_is ():
+    "function to show my name"
     indivi_name= "ParissaPeimaniyfard"
     return indivi_name
 
 
 def my_buddy_is ():
+    "function to show my buddy name"
     budd_name= "ParissaPeimaniyfard"
     return budd_name
